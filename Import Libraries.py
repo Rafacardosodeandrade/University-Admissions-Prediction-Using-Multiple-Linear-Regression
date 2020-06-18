146c57d589404c08d03f296e7fc3fbf2c4e6b0b0
Rafael <rafacardosodeandrade@gmail.com>
2020-06-18 13:31:11 -0500

    Libraries

--- Import Libraries.py
+++ Import Libraries.py
@@ -1,1 +1,6 @@
-testando
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from jupyterthemes import jtplot
+jtplot.style(theme='monokai' , context='notebook' , ticks=True, grid=False)
