b65ef8908d2b253c89e6795f3d985465ad365681
Rafael <rafacardosodeandrade@gmail.com>
2020-06-21 20:08:26 -0500

    Spliting the data

--- Import Libraries.py
+++ Import Libraries.py
@@ -85,5 +85,11 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 scaler_x = StandardScaler()
 x = scaler_x.fit_transform(x)
 
+scaler_y = StandardScaler()
+y = scaler_x.fit_transform(y)
 
+#spliting the data in to test and train sets
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split (x, y, test_size = 0.15)
 
